add data, fix singular jacobian model, clean up code

main.py
@@ -1,4 +1,5 @@
 from graph_helper import *
+import os
 
 def test_cycles_routine(n,q):
 	G, adj_matrix,lookup = generate_graph(n,q)
@@ -29,6 +30,7 @@ def test_cycles_routine(n,q):
 	print(i2)
 	r2,i2 = iter_newton(new_dfs_A,x2,psi2)
 	print(i2)
+
 
 def test_routine(minV,maxV,prob):
 	# 3 cycles graph
@@ -115,11 +117,18 @@ def shortesttest(minV,maxV,prob):
 	alpha3 = max(np.absolute(function(A,x3,psi3)))
 	r3,i3 = iter_newton(A,x3,psi3)
 
-def testfromfile(f):
-	G, adj_matrix,A, lookup,fedges = readfromfile(f)
+def readallfiles(filedirectory):
+	for filename in os.listdir(filedirectory):
+		testfromfile(filename,filedirectory)
+
+def testfromfile(f,filedirectory):
+	print(f)
+	fresults = open("result.txt", "a")
+	G, adj_matrix, A, lookup, fcycles, fedges = readfromfile(f,filedirectory)
 	#print(adj_matrix)
 	flow_value = 1
 	n, m = len(adj_matrix), len(lookup)
+	print("num vertices", n, " num edges: ", m)
 	k = m-n+1
 	source, sink = 0, n-1
 	outflow = {source: flow_value, sink: -flow_value}
@@ -134,15 +143,43 @@ def testfromfile(f):
 			if u<v:
 				edges.append((u,v))
 
+	#shortest path initialization
+	print("Shortest path init")
+	psi1 = shortest_path_init(G,source,sink,flow_value,lookup)
+	x1 = [0]*k
+	alpha1 = max(np.absolute(function(A,x1,psi1)))
+	r1,i1 = iter_newton(A,x1,psi1)
+
+	#minimize the maximum flow initialization
+	print("Minimize max flow init")
+	psi2 = min_max_flow_init(nodes, edges, outflow,lookup)
+	x2 = [0]*k
+	alpha2 = max(np.absolute(function(A,x2,psi2)))
+	r2,i2 = iter_newton(A,x2,psi2)
+	
+	
+	#laminar flow initialization
+	print("Laminar flow init")
+	psi3 = laminar_flow_init(nodes,edges,outflow,lookup)
+	x3 = [0]*k
+	alpha3 = max(np.absolute(function(A,x3,psi3)))
+	r3,i3 = iter_newton(A,x3,psi3)
+
 	#epanet init initialization
-	psi4 = epanet_init(nodes, edges, outflow, A, lookup,fedges)
+	print("Epanet init")
+	psi4 = epanet_init(nodes, edges, outflow, A, lookup, fedges)
 	x4 = [0]*k
 	alpha4 = max(np.absolute(function(A,x4,psi4)))
 	r4,i4 = iter_newton(A,x4,psi4)
-	print(i4)
+
+	result = [f,n,m,k, i1, i2, i3, i4, alpha1, alpha2, alpha3,alpha4]
+	fresults.write(','.join(str(i) for i in result))
+	fresults.write('\n')
+	fresults.close()
+	
 
 def main1():
-	f = open("result.txt", "a")
+	f = open("result_0503.txt", "a")
 	for n in range(10,200,10):
 		numtest = 10
 		prob = random.uniform(0.1,0.2)
@@ -160,15 +197,20 @@ def main1():
 		f.write('\n')
 	f.close()
 			
-def main():
+def randomgenerator():
 	#test_cycles_routine(10,0.3)
-	graph_counter = 1
-	for n in range(10,210,10):
+	graph_counter = 201
+	for n in range(210,510,10):
 		numtest = 10
-		prob = random.uniform(0.05,0.5)
+		prob = random.uniform(0.01,0.1)
 		for i in range(numtest):
 			G, adj_matrix, A, lookup, fcycles, fedges = random_generation_graph(n,n, prob, graph_counter)
 			graph_counter+=1
+
+def main():
+	#randomgenerator()
+	filedirectory = "data4_2/"
+	readallfiles(filedirectory)
 
 if __name__ == '__main__':
     main()

nr.py
@@ -3,25 +3,27 @@ import numpy as np
 from gurobipy import *
 
 def iter_newton(A,x,psi,imax=100,tol=1e-6):
-	iter = 1
-	bound = sum(v for v in psi if v>0)*len(A)*2
-	for i in range(imax):
+	iter = 0
+	#bound = sum(v for v in psi if v>0)*len(A)
+	bound = 2
+	#print(bound)
+	while iter<=imax:
 		J = jacobian(A,x,psi)
 		Y = function(A,x,psi)
-
-		#if Jacobian is singular
-		if np.linalg.matrix_rank(J) < len(x):
-			#print("i: ",i)
-			#print("not full rank")
-			x = solve_singular(J, Y, bound)
-			#print(x)
-		else:
-			dx = np.linalg.solve(J,-Y)
-			x = np.add(x,dx)
-		iter+=1
 		if np.linalg.norm(Y) < tol:
 			break
-	#print("numberof iterations ", iter)
+		#if Jacobian is singular
+		rankJ = np.linalg.matrix_rank(J)
+		if rankJ < len(x):
+			print("i: ",iter)
+			print("rank J: ", rankJ)
+			dx = solve_singular(J, Y, bound)
+		else:
+			dx = np.linalg.solve(J,-Y)
+		#print(np.add(np.dot(dx,J),Y))
+		x = np.add(x,dx)
+		#print(x)
+		iter+=1
 	return x,iter
 
 def ssquare(a):
@@ -43,7 +45,74 @@ def jacobian(A,x,psi):
 	J = 2*np.dot(np.dot(A.transpose(),diag_flows),A)
 	return J
 
-def solve_singular(J, Y, bound):
+def solve_singular(J,Y,bound):
+	m = Model('singular')
+	m.Params.LogToConsole = 0
+	#var
+	x = [i for i in range(len(Y))]
+	adjustments = m.addVars(x,name="adjustments", lb = -bound, ub = bound)
+	maxAdj = m.addVar(name="maxAdj")
+
+	eqs_idx = []
+	for i,row in enumerate(J):
+		if any(row)!=0:
+			eqs_idx.append(i)
+
+	#constraint Jx = -Y
+	for i in range(len(x)):
+		eq = LinExpr(J[i,:],[adjustments[j] for j in range(len(x))])
+		m.addConstr(eq==-Y[i])
+
+	#aux constraint max 
+	for i in range(len(x)):
+		m.addConstr(maxAdj>=adjustments[i])
+		m.addConstr(maxAdj>=-adjustments[i])
+
+	m.setObjective(maxAdj, GRB.MINIMIZE)
+	
+	m.update()
+	#m.display()
+	#m.write('model.lp')
+	m.optimize()
+	#print(m.status)
+	res = m.getAttr('x',adjustments)
+	return res.values()
+
+def solve_singular1(J,Y,bound):
+	m = Model('singular')
+	m.Params.LogToConsole = 0
+	#var
+	x = [i for i in range(len(Y))]
+	adjustments = m.addVars(x,name="adjustments", lb = -bound, ub = bound)
+	absAdj = m.addVars(x,name="absAdj")
+	maxAdj = m.addVar(name="maxAdj")
+
+	eqs_idx = []
+	for i,row in enumerate(J):
+		if any(row)!=0:
+			eqs_idx.append(i)
+
+	#constraint Jx = -Y
+	for i in range(len(x)):
+		eq = LinExpr(J[i,:],[adjustments[j] for j in range(len(x))])
+		m.addConstr(eq==-Y[i])
+
+	#aux constraint max 
+	for i in range(len(x)):
+		m.addConstr(absAdj[i]==abs_(adjustments[i]))
+		m.addConstr(maxAdj>=absAdj[i])
+
+	m.setObjective(maxAdj, GRB.MINIMIZE)
+	
+	m.update()
+	#m.display()
+	#m.write('model.lp')
+	m.optimize()
+	#print(m.status)
+	res = m.getAttr('x',adjustments)
+	return res.values()
+
+def solve_singular1(J, Y, bound):
 	#print('Singular J')
 	m = Model('singular')
 	m.Params.LogToConsole = 0
@@ -67,7 +136,7 @@ def solve_singular(J, Y, bound):
 	m.setObjective(maxAdj, GRB.MINIMIZE)
 	
 	m.update()
-	#m.display()
+	m.display()
 	m.optimize()
 	return adjustments.X
 